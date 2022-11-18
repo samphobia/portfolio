569cfe2817d6ae76c10e4c35f9cca8245d179b22
Samuel Osinubi <samobafemisayo@gmail.com>
2022-11-18 10:20:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header } from './components/home/header/Header'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Homepage } from './components/pageContent/Homepages'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+      <Header />
+      <Routes>
+      <Route  index element={<Homepage />} />
+      </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/pageContent/Homepages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+      
+      <div>
+        <h2 className='container'>homepages</h2>
+      </div>
+
+  )
+}
+
+// export default Homepage;
